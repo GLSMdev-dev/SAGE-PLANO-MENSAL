f519c39bdf7b8ff8da180d5ad94d87b1a6703955
GLSMdev-dev <guiluis.dev@gmail.com>
2026-03-28 12:08:59 -0300

    Create config-template.js

---
+++ assets/js/config-template.js
@@ -0,0 +1,11 @@
+// config-template.js
+// Modelo de configuração - será preenchido pelo GitHub Actions
+// NÃO EDITAR DIRETAMENTE
+
+window.APP_CONFIG = {
+    API_URL: '{{API_URL}}',
+    SPREADSHEET_ID: '{{SPREADSHEET_ID}}',
+    API_KEY: '{{API_KEY}}',
+    APP_NAME: 'SAGE Planos',
+    APP_VERSION: '1.0.0'
+};
